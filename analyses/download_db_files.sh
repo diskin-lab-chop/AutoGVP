7d76b5dd307bb2d70f351a1be2218aa12f19b28b
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-03-29 13:05:16 -0400

    changed clinvar version

--- analyses/download_db_files.sh
+++ analyses/download_db_files.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/archive_2.0/2023/clinvar_20230218.vcf.gz -P input/
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar.vcf.gz -P input/
 wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/submission_summary.txt.gz -P input/
 wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz -P input/
 
