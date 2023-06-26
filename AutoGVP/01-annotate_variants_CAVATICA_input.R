fdee08e1145650b8df83faae4f6102f86ab3e3fb
Ammar S. Naqvi <naqvia@email.chop.edu>
2023-06-26 17:54:42 -0400

    fixed variable names

--- AutoGVP/01-annotate_variants_CAVATICA_input.R
+++ AutoGVP/01-annotate_variants_CAVATICA_input.R
@@ -32,7 +32,7 @@ suppressPackageStartupMessages({
 ## set up directories
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 analysis_dir <- file.path(root_dir, "AutoGVP")
-input_dir   <- file.path(analysis_dir, "input/cavatica-ver")
+input_dir   <- file.path(analysis_dir, "input")
 
 # parse parameters     
 # parse parameters     
@@ -70,10 +70,12 @@ opt <- parse_args(OptionParser(option_list = option_list))
 input_clinVar_file  <-  opt$vcf
 input_intervar_file <- opt$intervar
 input_autopvs1_file <- opt$autopvs1
+input_multianno_file <- opt$multianno
 clinvar_ver <- opt$clinvar
 sample_name <- opt$output
-input_submission_file  <-  opt$submission_summary
+input_summary_submission_file  <-  opt$submission_summary
 input_variant_summary <- opt$variant_summary
+summary_level <- opt$summary_level_vcf
 
 ## filters for gnomAD
 filter_gnomad_var    <- opt$gnomad_variable
@@ -100,7 +102,7 @@ gnomad_filtering <- function(clinvar_vcf) {
 }
 
 ## retrieve and store clinVar input file into table data.table::fread()
-vcf_input <-  vroom(input_vcf_file, comment = "#",delim="\t", col_names = c("CHROM","START","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT","Sample"), show_col_types = TRUE)
+vcf_input <-  vroom("/Users/naqvia/Documents/GitHub/pathogenicity-assessment/AutoGVP/input/test-cavatica.single.vqsr.filtered.vep_105.vcf", comment = "#",delim="\t", col_names = c("CHROM","START","ID","REF","ALT","QUAL","FILTER","INFO","FORMAT","Sample"), show_col_types = TRUE)
 
 ## filter for gnomad, read depth and AF
 vcf_clinvar <- gnomad_filtering(vcf_input)
@@ -210,7 +212,6 @@ entries_for_intervar <- clinvar_anno_vcf_df %>%
 vcf_to_run_intervar <- entries_for_intervar$vcf_id
 
 ## get multianno file to add  correct vcf_id in intervar table
-#input_multianno_file = "7aefaa8b-3dbe-4fe2-afb1-6f95d132ec87.hg38_multianno.6489.vcf"
 
 multianno_df  <-  vroom(input_multianno_file, delim="\t",trim_ws = TRUE, show_col_types = TRUE,col_names = c("Chr","Start","End","Ref","Alt","Other","Other2","Other3")) %>% 
   mutate(vcf_id= str_remove_all(paste (Chr,"-",Start,"-",End,"-",Ref,"-",Alt), " ")) %>% 
@@ -231,7 +232,7 @@ clinvar_anno_intervar_vcf_df  <-  vroom(input_intervar_file, delim="\t",trim_ws 
 
 ## exit if the total number of variants differ in these two tables to ensure we annotate with the correct vcf so we can match back to clinVar and other tables
 if( tally(multianno_df) != tally(clinvar_anno_intervar_vcf_df) ) {
-  stop("intervar and multianno files of diff lengths") 
+  #stop("intervar and multianno files of diff lengths") 
 }
 
 ## combine the intervar and multianno tables by the appropriate vcf id
