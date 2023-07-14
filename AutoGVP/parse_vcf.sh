7c62fdddfea339f12979b71934c58883f56799bd
rjcorb <ryan@d3b.center>
2023-07-14 19:32:58 +0000

    annotate bash script

--- AutoGVP/parse_vcf.sh
+++ AutoGVP/parse_vcf.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-vcf_file=$1
+vcf_file=test_VEP.vcf
 
 ## name output file
 vcf_parsed_file=${vcf_file%.vcf*}."parsed.vcf"
 
+## Extract list of subfields in INFO column
 egrep -v "^#" $vcf_file | head -n 2 | awk '{ n=split($8, tmp, /=[^;]*;/); for(i=1; i<n; i++) print tmp[i] }' > subfields.tsv
 
 subfields=$(cat subfields.tsv)
 
+# Call function to join subfields 
 function join_by {
   local d=${1-} f=${2-}
   if shift 2; then
@@ -15,8 +17,11 @@ function join_by {
   fi
 }
 
+# Join subfields by '\t%' for bcftools query
 subfield_list=$(join_by '\t%' $subfields)
 
+# Add standard vcf columns to list to parse
 all_columns="%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%${subfield_list}\n"
 
+# run vcftools query to parse all columns and info subfields, and include header
 bcftools query -H -f $all_columns $vcf_file > $vcf_parsed_file
