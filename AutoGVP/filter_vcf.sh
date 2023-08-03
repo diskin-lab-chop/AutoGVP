5577c10564316482273858a6bb2d65cd2cadd763
rjcorb <ryan@d3b.center>
2023-08-03 17:11:57 +0000

    add out prefix, outdir parameter

--- AutoGVP/filter_vcf.sh
+++ AutoGVP/filter_vcf.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 vcf_file=$1
+out_file=$2
+out_dir=$3
 exp_args=("$@")
 
+
 ## get vcf filename
-vcf_filtered_file=${vcf_file%.vcf*}."filtered.vcf"
+# vcf_filtered_file=${vcf_file%.vcf*}."filtered.vcf"
+vcf_filtered_file=${out_file}."filtered.vcf"
 
 echo "vcf file: $vcf_file ";
-
-
 
 # default filters
 cmd="bcftools filter -i 'INFO/AF >= 0.2' $vcf_file | bcftools filter -i 'INFO/DP >=15' | bcftools view -f PASS"
 
 ## loop through args for other user-defined filters
-for i in "${exp_args[@]:1}"; do
+for i in "${exp_args[@]:3}"; do
   #echo filtering for... $i
   cmd+=" | bcftools filter -i '$i'"
 #  cmd+=" $vcf_file "
   #echo $cmd
 done
 
-cmd+=" > $vcf_filtered_file"
+cmd+=" > $out_dir/$vcf_filtered_file"
 
 echo "cmd: " $cmd
 eval "$cmd"
