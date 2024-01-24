b8d9f5a21bdabcdaef15ba0d7f30fa146e48202a
rjcorb <ryan@d3b.center>
2024-01-24 18:59:10 +0000

    fix typo

--- scripts/02-annotate_variants_CAVATICA_input.R
+++ scripts/02-annotate_variants_CAVATICA_input.R
@@ -61,7 +61,7 @@ option_list <- list(
 
 opt <- parse_args(OptionParser(option_list = option_list))
 
-## get input files from parameters (reqd)
+## get input files from parameters (read)
 input_vcf_file <- opt$vcf
 input_intervar_file <- opt$intervar
 input_autopvs1_file <- opt$autopvs1
