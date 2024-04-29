dca4c37f1dbef6a207b64714dcfea409d020c2cb
rjcorb <rjcorb@users.noreply.github.com>
2024-04-29 19:27:23 +0000

    Apply code style changes

--- scripts/04-filter_gene_annotations.R
+++ scripts/04-filter_gene_annotations.R
@@ -194,18 +194,18 @@ split_and_unique <- function(string) {
 # Coalesce rsIDs across multiple columns, when present
 id_df <- merged_df %>%
   dplyr::select(any_of(c("ID", "avsnp147", "Existing_variation"))) %>%
-#  dplyr::mutate(across(everything(), ~ ifelse(grepl("rs", .x), .x, NA_character_))) %>%
-#  dplyr::mutate(variantID = coacross()) %>%
-#  dplyr::mutate(variantID = str_replace(variantID, "&", ";")) %>%
-#  dplyr::mutate(variantID = str_replace_all(variantID, "~", ""))
-#  dplyr::mutate(across(everything(), as.character)) %>%
+  #  dplyr::mutate(across(everything(), ~ ifelse(grepl("rs", .x), .x, NA_character_))) %>%
+  #  dplyr::mutate(variantID = coacross()) %>%
+  #  dplyr::mutate(variantID = str_replace(variantID, "&", ";")) %>%
+  #  dplyr::mutate(variantID = str_replace_all(variantID, "~", ""))
+  #  dplyr::mutate(across(everything(), as.character)) %>%
   dplyr::mutate(across(everything(), ~ ifelse(.x %in% c(".", NA_character_), "", .x))) %>%
   rowwise() %>%
   mutate(variantID = glue::glue(paste(c_across(everything()), collapse = ";"))) %>%
   ungroup() %>%
   dplyr::mutate(variantID = unlist(lapply(variantID, split_and_unique))) %>%
   dplyr::mutate(variantID = case_when(
-    variantID == "NA" ~ "", 
+    variantID == "NA" ~ "",
     TRUE ~ variantID
   )) %>%
   dplyr::mutate(variantID = str_replace_all(variantID, "~", ""))
